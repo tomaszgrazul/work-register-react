ba080191bc37370f043415598a461303b8f2765f
tomaszgrazul <tgra@wp.pl>
2023-07-13 10:04:56 +0200

    po rozdzieleniu na dwa repo

---
+++ src/WorkRegister.js
@@ -0,0 +1,208 @@
+import { useState, useEffect} from "react";
+import './WorkRegister.css';
+import axios from "axios";
+import ModalCompanyName from "./components/modalCompanyName";
+
+
+const WorkRegister = () => {
+
+     const [openModalCompanyName, setOpenModalCompanyName] = useState(false);
+    const [companyName, setCompanyName] = useState('');
+    const [errors, setErrors] = useState({
+        companyName: ''
+    });
+    const [companyNumber, setCompanyNumber] = useState();
+
+
+
+    const handleCompanyList = (e) => {
+        
+        setCompanyName(e.target.value);
+    }
+
+    const handleAddCompanyName = (addCompanyName, isChecked, i) => {
+                
+        !isChecked[i]? setCompanyName(addCompanyName) : setCompanyName('');    
+    }
+
+    const handleReadCompanyNumber = () => {
+
+        readCompanyNumber(); 
+    }
+
+    const readCompanyNumber = () => {
+
+        axios
+        // .post("http://127.0.0.1:8080/readCount") 
+        .get("http://127.0.0.1:8080/readCount")
+        .then((res) => { 
+            setCompanyNumber(res.data);       
+        })
+        .catch((error) => {
+            console.error(error);
+        }); 
+    }
+
+    useEffect(() => {
+
+        readCompanyNumber(); 
+        // console.log("companyNumber",companyNumber );
+    }, []);
+
+
+    const addCompany = () => {
+       
+ 
+        let newCompany = {
+            _id: Date.now(),
+            companyName: companyName
+        }
+
+        if (newCompany.companyName === '') {    
+            setErrors(() => {
+                return {
+                    companyName: "Wpisz nazwę firmy !!!"
+                };
+            });
+            return;
+        } else setErrors('');
+
+        setCompanyName('');
+
+        axios
+        .post("http://127.0.0.1:8080/create", newCompany)
+        .then((res) => {
+            readCompanyNumber(); 
+         })
+        .catch((error) => {
+            console.error(error);
+        });
+    };
+
+
+    return (
+        <div className="register-main">
+            <header>
+                <h1>Prace szczególnie niebezpieczne</h1>
+            </header>
+            <form>
+                <div>
+                    <div className="label">
+                        <label htmlFor="praceRealizuje">Prace realizuje</label>
+                    </div> 
+                    <input type="text" placeholder="" name="praceRealizuje" />
+                </div>
+                
+                <div>
+                    <div className="label">
+                        <label htmlFor="nrPorozumienia">Nr porozumienia</label>
+                    </div> 
+                    <input type="text" placeholder="" name="nrPorozumienia" />
+                    <button>Wybierz</button>
+                </div>
+                
+                <div>
+                    <div className="label">
+                        <label htmlFor="nrFirmyZewnetrznej">Nr firmy zew.</label>
+                    </div> 
+                    <input type="text" placeholder="" name="nrFirmyZewnetrznej" />
+                </div>
+
+                <div>
+                    <div className="label">
+                        <label htmlFor="companyName">Nazwa firmy</label>
+                    </div> 
+                    <input onChange={handleCompanyList}  value={companyName} type="text" placeholder="" name="companyName" />
+                    <button onClick={(e) => {
+                        e.preventDefault();
+                        addCompany();
+                    }}>Dodaj</button>
+                    <button onClick={(e) => {
+                        e.preventDefault();
+                        setOpenModalCompanyName(true);
+                    }}>Wybierz</button>
+                    {errors.companyName && <p className="error">{errors.companyName}</p>}
+                </div>
+
+                <div>
+                    <div className="label">
+                        <label htmlFor="nazwaBiura">Nazwa biura</label>
+                    </div>  
+                    <input type="text" placeholder="" name="nazwaBiura" />
+                    <button>Wybierz</button>
+                </div>
+
+                <div>
+                    <div className="label">
+                        <label htmlFor="poleceniodawca">Poleceniodawca</label>
+                    </div>   
+                    <input type="text" placeholder="" name="poleceniodawca" />
+                    <button>Wybierz</button>
+                </div>
+
+                <div>
+                    <div className="label">
+                        <label htmlFor="koordynujący">Koordynujący</label>
+                    </div>    
+                    <input type="text" placeholder="" name="koordynujący" />
+                    <button>Wybierz</button>
+                </div>
+
+                <div>
+                    <div className="label">
+                        <label htmlFor="koordynator">Koordynator</label>
+                    </div>  
+                    <input type="text" placeholder="" name="koordynator" />
+                    <button>Wybierz</button>
+                </div>
+
+                <div>
+                    <div className="label">
+                        <label htmlFor="dopuszczający">Dopuszczający</label>
+                    </div>   
+                    <input type="text" placeholder="" name="dopuszczający" />
+                    <button>Wybierz</button>
+                </div>
+
+                <div>
+                    <div className="label">
+                        <label htmlFor="kierującyZespołem">Kierujący zespołem</label>
+                    </div> 
+                    <input type="text" placeholder="" name="kierującyZespołem" />
+                    <button>Wybierz</button>
+                </div>
+
+                <div>
+                    <div className="label">
+                        <label htmlFor="nadzorEksploatacyjny">Nadzór eksploatacyjny</label>
+                    </div>     
+                    <input type="text" placeholder="" name="nadzorEksploatacyjny" />
+                    <button>Wybierz</button>
+                </div>
+
+                <div>
+                    <label htmlFor="rozpoczeciePracy">Rozpoczęcie pracy (data, godzina)</label>
+                    <input type="datetime-local" placeholder="" name="rozpoczeciePracy" />
+                </div>
+
+                <div>
+                    <label htmlFor="zakonczeniePracy">Zakończenie pracy (data, godzina)</label>
+                    <input type="datetime-local" placeholder="" name="zakonczeniePracy" />
+                </div>
+
+
+
+                
+                
+
+            </form>
+            {openModalCompanyName && <ModalCompanyName setModalCompanyName={setOpenModalCompanyName} handleAddCompanyName={handleAddCompanyName} companyNumber={companyNumber} handleReadCompanyNumber={handleReadCompanyNumber}/>}
+        </div>
+
+    )
+}
+
+
+
+
+export default WorkRegister;
---
+++ src/components/modalCompanyName.js
@@ -0,0 +1,104 @@
+import './modalCompanyName.css'
+import axios from "axios";
+import { useState, useEffect } from "react";
+
+function ModalCompanyName({setModalCompanyName, handleAddCompanyName, companyNumber, handleReadCompanyNumber}) {
+
+ 
+    const [register, setRegister] = useState([]);
+    const [isChecked, setIsChecked] = useState(new Array(companyNumber).fill(false));
+
+    const checkHandler = (i) => {
+        setIsChecked(isChecked.map((item, index) => {
+            if( index === i) {
+                return item = !item;
+            } else 
+                return item = item;
+        }));
+      }
+
+    const readCompanyList = () => {
+
+        axios
+        // .post("http://127.0.0.1:8080/index") 
+        .get("http://127.0.0.1:8080/index") 
+        .then((res) => { 
+            setRegister(res.data);       
+        })
+        .catch((error) => {
+            console.error(error);
+        });   
+    }
+
+    useEffect(() => {
+
+        readCompanyList();  
+    }, []);
+
+    // useEffect(() => {
+        
+    //     console.log("isChecked=", isChecked); 
+    //     // console.log("registerLength=", registerLength); 
+    //     // console.log("companyNumber z modala=", companyNumber); 
+    // }, [isChecked, companyNumber]);
+
+
+    
+
+    return (
+            <div className="modalCompanyName">
+                <div className='topModal'>
+                    <h3>Lista firm</h3>
+                    <div>
+                        {/* <p className='ex-modal' onClick={() => {setModalCompanyName(false); setRegisterIsChecked([]);}}>X</p> */}
+                        <p className='ex-modal' onClick={() => {setModalCompanyName(false)}}>X</p>
+                    </div>
+                    {/* <p>ala{register.length}</p> */}
+                </div>
+                <table>
+                    <tbody>
+                        <tr><th></th><th className="name">Firma</th><th className="action">Czynność</th></tr>
+                        {register.map((item, i) => {
+                                return (
+                                    <tr key={i}><td><input type="checkbox" className="checkbox" checked={isChecked[i]}
+                                        onChange={() => {
+                                            isChecked.map((item, index) => {
+                                                if( item ) {
+                                                    isChecked[i] = true;
+                                                } 
+                                                return checkHandler(i);
+                                            })
+                                            handleAddCompanyName(item.companyName, isChecked, i); 
+                                        }} 
+                                        /></td><td className="name">{item.companyName}</td><td className="action">
+                                        <button onClick={() => {
+
+                                                var filtered = register.filter((el, i) =>
+                                                    i !== register.findIndex((el) => el === item)
+                                                );
+                                                setRegister(filtered);
+                                                
+
+                                                axios
+                                                // .post("http://127.0.0.1:8080/delete", {id: item._id}) 
+                                                .delete(`http://127.0.0.1:8080/delete/${item._id}`) 
+                                                .then((res) => {
+                                                    handleReadCompanyNumber();
+                                                     })
+                                                .catch((error) => {
+                                                    console.error(error);
+                                                });       
+                                            }}
+                                            className="btn-delete">Usuń
+                                        {/* </button><button onClick={(e) => {handleAddCompanyName(item.companyName)}}>Wybierz</button></td></tr> */}
+                                        </button></td></tr>
+                                )      
+                            })}    
+                    </tbody>
+                </table>
+                {/* <button className="btn-modal" onClick={() => setModalCompanyName(false)} >Anuluj</button> */}
+            </div>
+        )
+}
+
+export default ModalCompanyName;
