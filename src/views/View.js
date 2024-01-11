7c2560c48c590825fdafecd8094d6ee0ffe7ac5b
tomaszgrazul <tgra@wp.pl>
2024-01-11 16:03:33 +0100

    dodano zakładkę widok

--- src/views/View.js
+++ src/views/View.js
@@ -1,19 +1,39 @@
 import './View.css'
+import logo from "../img/logo_EG.png"
 
 
 const View = () => {
 
 
 return (
-    <div className="headerView">
-        <div className='asa'>
-            <div className='regNumber'>Nr w rejestrze EuRoPolGaz 23/DEZ/2024</div>
-            <div className='orderNumber'>Polecenie pisemne nr 01/01/2024</div>
+    <div className="headerMain">
+        <div className='header1'> 
+            <img src={logo} alt="logo" />
         </div>
-        <div className='orderDate'>Data: 05.01.2024</div>
-        <div className='orderHour'>Godzina: 11.00</div>
-        <div className='headerMainText'>
-            <h2>Polecenie pisemne wykonania prac przy urządzeniach energetycznych</h2>
+        <div className='header2'>
+            <div className='header21'>
+                <p className='p1'>Zał. nr 7 do EG 050801</p>
+                <p className='regNumber'>Nr w rejestrze EuRoPolGaz: 23/DEZ/2024</p>
+                <div className='header211'>
+                    <p>Polecenie pisemne nr</p>
+                    <p className='orderNumber'>01/01/2024</p>
+                </div>
+            </div>
+            <div className='header22'>
+                <p className='p2'>Wyd. 02</p>
+                <p className='p3'>(wypełnia wykonawca zewnętrzny)</p>
+                <div className='header221'>
+                    <p>Data:</p>
+                    <p className='orderDate'>05.01.2024</p>
+                </div>
+            </div>
+            <div className='header23'>
+                <p>Godzina:</p>
+                <p className='orderHour'>11.00</p>
+            </div>
+            <div className='headerMainText'>
+                <h2>Polecenie pisemne wykonania prac przy urządzeniach energetycznych</h2>
+            </div>
         </div>
     </div>
 )
